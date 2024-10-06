52f397dee42f18bb757e6d252367ec3105ad92bc
Jaxon Adams <jadamsresume1@gmail.com>
2024-10-06 00:19:22 -0600

    Draw player to the screen
    The code drawing the player to the screen currently lives in the same
    function that draws the game map. This will probably change in the
    future once I've played around with the code a bit more.

--- src/setup.ts
+++ src/setup.ts
@@ -12,14 +12,31 @@ const configureCanvas = (canvas: HTMLCanvasElement): CanvasConfig => {
 };
 
 const initialDraw = (ctx: CanvasRenderingContext2D, width: number, height: number) => {
-    ctx.fillStyle = "#67e6d2";
+    ctx.fillStyle = "#67e6d2";  // same color as the ocean on our map
     ctx?.fillRect(0, 0, width, height);
     
     const gameMap = new Image();
     gameMap.src = "src/assets/game_map.png";
     
+    const playerImg = new Image();
+    playerImg.src = "src/assets/playerDown.png";
+
+    // game map will load before the player image
     gameMap.onload = () => {
-      ctx?.drawImage(gameMap, -900, -500);
+      ctx?.drawImage(gameMap, -950, -400);
+      playerImg.onload = () => {
+        ctx?.drawImage(
+            playerImg,                         // image
+            0,                                 // crop x start
+            0,                                 // crop y start
+            playerImg.width / 4,               // crop width
+            playerImg.height,                  // crop height
+            width / 2 - playerImg.width / 8,   // x position
+            height / 2 - playerImg.height / 8, // y position
+            playerImg.width / 4,               // width
+            playerImg.height                   // height
+        );
+      };
     };
 };
 
